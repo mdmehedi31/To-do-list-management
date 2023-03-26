33bd7a864b8d929a8b764029ff04187c52805182
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2023-03-27 01:46:14 +0600

    Code refactoring

---
+++ frontend/src/app/Users/users1/users1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users1',
+  templateUrl: './users1.component.html',
+  styleUrls: ['./users1.component.css']
+})
+export class Users1Component {
+
+}
---
+++ frontend/src/app/admin/todo1/todo1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo1',
+  templateUrl: './todo1.component.html',
+  styleUrls: ['./todo1.component.css']
+})
+export class Todo1Component {
+
+}
